bf95e38be29135b8c04aeb639804bf19c8981a5a
Malebestia <vair81@gmail.com>
2024-01-05 18:03:27 +0100

    prova inserimento FilamentPeekPlugin

--- Providers/Filament/AdminPanelProvider.php
+++ Providers/Filament/AdminPanelProvider.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 namespace Modules\Blog\Providers\Filament;
 
+use Filament\Panel;
+use Pboivin\FilamentPeek\FilamentPeekPlugin;
 use Modules\Xot\Providers\Filament\XotBasePanelProvider;
 
 class AdminPanelProvider extends XotBasePanelProvider
 {
     protected string $module = 'Blog';
+
+    // public function panel(Panel $panel): Panel
+    // {
+    //     return parent::panel($panel)->plugins([
+    //         FilamentPeekPlugin::make()
+    //     ]);
+    // }
 }
