8eb25e89d8021a53be6a9d3e5e633836ba51dca1
marco76tv <marco.sottana@gmail.com>
2024-01-16 13:56:31 +0100

    up

--- Config/config.php
+++ Config/config.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 return [
     'name' => 'Blog',
-    'icon' => 'heroicon-o-cog', // icon on dashboard
+    // 'icon' => 'heroicon-o-cog', // icon on dashboard
+    'icon' => 'fas-blog', // icon on dashboard
     'navigation_sort' => 1,
 ];
