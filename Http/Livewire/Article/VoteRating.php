e4f41c3e9e2da793e8c1563d966507af744e1b97
marco76tv <marco.sottana@gmail.com>
2024-01-16 11:43:59 +0100

    componente Article/VoteRating test

---
+++ Http/Livewire/Article/VoteRating.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Blog\Http\Livewire\Article;
+
+use Livewire\Component;
+use Modules\Blog\Models\Article;
+use Modules\Rating\Models\Rating;
+use Modules\Xot\Actions\GetViewAction;
+use Illuminate\Contracts\Support\Renderable;
+
+class VoteRating extends Component
+{
+    public string $tpl = 'v1';
+
+    public Article $article;
+
+    public function mount($article): void
+    {
+        $this->article = $article;
+    }
+
+    public function render(): Renderable
+    {
+        /**
+         * @phpstan-var view-string
+         */
+        $view = app(GetViewAction::class)->execute($this->tpl);
+        // dddx($view);
+        $view_params = [
+            'article' => $this->article,
+        ];
+
+        return view($view, $view_params);
+    }
+
+    public function vote(Rating $rating){
+        $this->article->ratings()->attach($rating->id, ['user_id' => \Auth::id()]);
+        dddx($rating);
+    }
+}
--- Resources/views/components/blocks/rating.blade.php
+++ Resources/views/components/blocks/rating.blade.php
@@ -1,9 +1,13 @@
 <div>
-    {{-- dddx($model->ratings) --}}
+    {{-- {{ dddx($model->ratings) }} --}}
     WIP
     <ul>
+        <livewire:article.vote-rating :article="$model" />
+    </ul>
+
+    {{-- <ul>
     @foreach($model->ratings as $rating)
         <li>{{ $rating->title }}</li>
     @endforeach
-    </ul>
+    </ul> --}}
 </div>
