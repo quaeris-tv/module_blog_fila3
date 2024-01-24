31470a8f33477cd7d3dbf21ef06038fa076dc645
marco76tv <marco.sottana@gmail.com>
2024-01-24 13:59:49 +0100

    up

---
+++ Filament/Widgets/LatestArticlesWidget.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Blog\Filament\Widgets;
+
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class LatestArticlesWidget extends BaseWidget
+{
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                // ...
+            )
+            ->columns([
+                // ...
+            ]);
+    }
+}
