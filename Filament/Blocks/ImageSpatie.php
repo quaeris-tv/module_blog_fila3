1dd3572e529da248d7f863c9ebc389712a59554b
marco76tv <marco76tv@users.noreply.github.com>
2024-02-08 08:38:26 +0000

    Lint

--- Filament/Blocks/ImageSpatie.php
+++ Filament/Blocks/ImageSpatie.php
@@ -5,12 +5,9 @@ declare(strict_types=1);
 namespace Modules\Blog\Filament\Blocks;
 
 use Filament\Forms;
-use Illuminate\Support\Str;
-use Spatie\MediaLibrary\HasMedia;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Builder\Block;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Illuminate\Support\Str;
 
 class ImageSpatie
 {
@@ -20,25 +17,21 @@ class ImageSpatie
     ): Block {
         return Block::make($name)
             ->schema([
-
                 Forms\Components\Hidden::make('img_uuid')
-                    ->default(fn() => Str::uuid()->toString())
-                    ->formatStateUsing(fn($state) => $state ?? Str::uuid()->toString())
+                    ->default(fn () => Str::uuid()->toString())
+                    ->formatStateUsing(fn ($state) => $state ?? Str::uuid()->toString())
                     ->live(),
-                    //->required(),
+                // ->required(),
 
-                Forms\Components\SpatieMediaLibraryFileUpload::make('image')
-                    ->collection(fn(Forms\Get $get) => $get('img_uuid'))
+                SpatieMediaLibraryFileUpload::make('image')
+                    ->collection(fn (Forms\Get $get) => $get('img_uuid')),
 
+                // Filament\Forms\Components\SpatieMediaLibraryFileUpload::whereCustomProperties does not exist.
+                // ->whereCustomProperties(fn(Forms\Get $get) => ['gallery_id' => $get('gallery_id')])
 
-                    //Filament\Forms\Components\SpatieMediaLibraryFileUpload::whereCustomProperties does not exist.
-                    //->whereCustomProperties(fn(Forms\Get $get) => ['gallery_id' => $get('gallery_id')])
+                // ->customProperties(fn(Forms\Get $get) => ['gallery_id' => $get('gallery_id')]),
 
-                    //->customProperties(fn(Forms\Get $get) => ['gallery_id' => $get('gallery_id')]),
-
-                //Forms\Components\SpatieMediaLibraryFileUpload::make('media_id')
-
-
+                // Forms\Components\SpatieMediaLibraryFileUpload::make('media_id')
             ])
             ->columns('form' === $context ? 2 : 1);
     }
