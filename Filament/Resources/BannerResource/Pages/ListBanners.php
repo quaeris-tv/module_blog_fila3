16c5cf688370ca151026b4d762744db6b8e9cfef
marco76tv <marco.sottana@gmail.com>
2024-02-29 10:57:00 +0100

    up

--- Filament/Resources/BannerResource/Pages/ListBanners.php
+++ Filament/Resources/BannerResource/Pages/ListBanners.php
@@ -1,10 +1,19 @@
 <?php
+/**
+ * @see https://packagist.org/packages/owainjones74/filament-file-upload-chunked
+ */
+
+declare(strict_types=1);
 
 namespace Modules\Blog\Filament\Resources\BannerResource\Pages;
 
-use Modules\Blog\Filament\Resources\BannerResource;
 use Filament\Actions;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Textarea;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\File;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+use Modules\Blog\Filament\Resources\BannerResource;
 
 class ListBanners extends ListRecords
 {
@@ -14,6 +23,25 @@ class ListBanners extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            Actions\Action::make('import')
+                    ->form([
+                        FileUpload::make('file')
+                            ->label('')
+                            ->acceptedFileTypes(['application/json', 'json'])
+                            ->imagePreviewHeight('250')
+                            ->reactive()
+                            ->afterStateUpdated(static function (callable $set, TemporaryUploadedFile $state) {
+                                $set('fileContent', File::get($state->getRealPath()));
+                            }),
+                        Textarea::make('fileContent'),
+                    ])
+                    ->label('')
+                    ->tooltip('Import')
+                    ->icon('heroicon-o-folder-open')
+                    ->action(static function ($data) {
+                        $json = json_decode($data['fileContent'], true);
+                        dddx($json);
+                    }),
         ];
     }
 }
