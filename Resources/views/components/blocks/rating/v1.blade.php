e172bf4767a564867eb833dd347ae5f2e68ffec6
marco76tv <marco.sottana@gmail.com>
2024-03-06 15:26:15 +0100

    .

--- Resources/views/components/blocks/rating/v1.blade.php
+++ Resources/views/components/blocks/rating/v1.blade.php
@@ -2,6 +2,7 @@
     'tpl',
     'version' => 'v1',
 ])
+
 <div>
-  <livewire:article.ratings :article="$model" :tpl="$version"/>
+  <livewire:article.ratings :article="$model" :tpl="$_tpl"/>
 </div>
