bd1bf8c5e867dbd5718fd6c8a6d8506d279ac1d1
marco76tv <marco76tv@users.noreply.github.com>
2024-03-08 09:42:29 +0000

    Lint

--- Filament/Fields/PageContent.php
+++ Filament/Fields/PageContent.php
@@ -15,9 +15,9 @@ use Modules\Blog\Filament\Blocks\Leaderboard;
 use Modules\Blog\Filament\Blocks\Paragraph;
 use Modules\Blog\Filament\Blocks\Search;
 use Modules\Blog\Filament\Blocks\Setting;
-use Modules\UI\Filament\Blocks\Title;
 use Modules\Rating\Filament\Blocks\Rating;
 use Modules\UI\Filament\Blocks\Slider;
+use Modules\UI\Filament\Blocks\Title;
 
 class PageContent
 {
