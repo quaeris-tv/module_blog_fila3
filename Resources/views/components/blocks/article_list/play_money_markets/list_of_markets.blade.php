b0f45f63d968759123f8335ada5404c6c8d8e27d
Malebestia <vair81@gmail.com>
2024-04-12 18:05:28 +0200

    .

--- Resources/views/components/blocks/article_list/play_money_markets/list_of_markets.blade.php
+++ Resources/views/components/blocks/article_list/play_money_markets/list_of_markets.blade.php
@@ -1,6 +1,10 @@
 <div class="py-4">
     @php
-        $articles = $_theme->getMethodData($method);
+        if(!isset($method)){
+            $articles = $_theme->getArticlesLatest();
+        }else{
+            $articles = $_theme->getMethodData($method);
+        }
         // dddx($articles);
     @endphp
 
