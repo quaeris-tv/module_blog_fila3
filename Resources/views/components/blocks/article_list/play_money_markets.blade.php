1daf2f6973867ba4e0a03bd3ec22f38d6648089a
rasyidly <rasyid@sekeco.id>
2024-04-13 16:57:12 +0700

    wip articles

--- Resources/views/components/blocks/article_list/play_money_markets.blade.php
+++ Resources/views/components/blocks/article_list/play_money_markets.blade.php
@@ -1,28 +1,24 @@
-<div class="max-w-[calc(100%-30px)] sm:max-w-[calc(100%-80px)] lg:max-w-[996px] mx-auto pb-12">
-	<section class="pt-8" aria-label="Play Money Markets" x-data="playmarkets" id="playmarkets">
-		<div class="flex justify-between items-center gap-4">
-			<div class="flex items-center gap-4">
-				<p class="text-neutral-5 text-base font-semibold">
-					Play Money Markets
-				</p>
-				<span class="text-sm text-neutral-3" x-text="markets.length+' Markets'"></span>
+<div class="py-8 mt-8 bg-white">
+	<div class="container max-w-6xl p-6 mx-auto space-y-8" x-data="playmarkets" id="playmarkets">
+		<h2 class="flex items-center space-x-2 text-xl font-semibold">
+			<x-heroicon-o-play-circle class="text-blue-500 size-8"/>
+			<div class="flex flex-wrap gap-x-2">
+				<span>Play Money Markets</span>
+				<span class="mt-1 text-sm font-normal text-gray-500" x-text="markets.length+' Markets'"></span>
 			</div>
-			<!-- order list -->
-			@include('blog::components.blocks.article_list.play_money_markets.order_select')
-
-		</div>
-		<!-- filterlist -->
-		{{-- @include('blog::components.blocks.article_list.play_money_markets.filter_list') --}}
-
-		<!-- list of markets -->
-
-		@include('blog::components.blocks.article_list.play_money_markets.list_of_markets')
-
-		<div class="py-12 flex justify-center">
-			<button type="button"
-				class="max-w-[320px] w-[90%] bg-white px-10 h-14 rounded-lg text-blue-1 hover:text-[#0f79c8]">
-			Load more
-			</button>
-		</div>
-	</section>
+		</h2>
+		<section class="space-y-4">
+			<div class="flex flex-wrap justify-between gap-2 gap-4 lg:items-center">
+				@include('blog::components.blocks.article_list.play_money_markets.filter_list')
+				@include('blog::components.blocks.article_list.play_money_markets.order_select')
+			</div>
+			@include('blog::components.blocks.article_list.play_money_markets.list_of_markets')
+			<div class="flex justify-center">
+				<button type="button" class="flex items-center px-4 py-2 space-x-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-600">
+					<span>Load more</span>
+					<x-heroicon-o-arrow-right class="size-4"/>
+				</button>
+			</div>
+		</section>
+	</div>
 </div>
