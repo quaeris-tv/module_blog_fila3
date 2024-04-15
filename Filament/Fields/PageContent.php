b73249226bc96a55d1b903b18b1125d1d9476f07
Malebestia <vair81@gmail.com>
2024-04-15 12:14:43 +0200

    fix PageContent

--- Filament/Fields/PageContent.php
+++ Filament/Fields/PageContent.php
@@ -12,7 +12,7 @@ use Modules\Blog\Filament\Blocks\Filter;
 use Modules\Blog\Filament\Blocks\Image;
 use Modules\Blog\Filament\Blocks\ImagesGallery;
 use Modules\Blog\Filament\Blocks\Leaderboard;
-use Modules\Blog\Filament\Blocks\Paragraph;
+use Modules\UI\Filament\Blocks\Paragraph;
 use Modules\Blog\Filament\Blocks\Search;
 use Modules\Blog\Filament\Blocks\Setting;
 use Modules\Rating\Filament\Blocks\Rating;
