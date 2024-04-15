a44949b61cb25334d96fcad6877ff8c74533dcbe
marco76tv <marco76tv@users.noreply.github.com>
2024-04-15 07:24:26 +0000

    Lint

--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -7,11 +7,11 @@ namespace Modules\Blog\Providers;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Logout;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Modules\User\Listeners\LoginListener;
 use Modules\User\Listeners\LogoutListener;
 use SocialiteProviders\Auth0\Auth0ExtendSocialite;
 use SocialiteProviders\Manager\SocialiteWasCalled;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
