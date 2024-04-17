2f19db186ac1a73a328e3dcd0f215791d4dc4891
marco76tv <marco76tv@users.noreply.github.com>
2024-04-17 17:12:07 +0000

    Lint

--- Models/Article.php
+++ Models/Article.php
@@ -317,7 +317,7 @@ class Article extends BaseModel implements Feedable, HasMedia // , Searchable
 
         foreach ($ratings as $key => $rating) {
             $ratings_array[$key] = $rating->toArray();
-            if(empty($rating->getFirstMediaUrl('rating'))) {
+            if (empty($rating->getFirstMediaUrl('rating'))) {
                 $rating->addMediaFromUrl('https://picsum.photos/id/'.$rating->id.'/300/200')
                        ->toMediaCollection('rating');
             }
