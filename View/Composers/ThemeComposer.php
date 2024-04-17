c20a63ddaeecf4a72c2e763527582469215f1cfc
marco76tv <marco.sottana@gmail.com>
2024-04-17 19:31:38 +0200

    rebase 4

--- View/Composers/ThemeComposer.php
+++ View/Composers/ThemeComposer.php
@@ -338,6 +338,7 @@ class ThemeComposer
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     /**
      * Undocumented function.
      *
@@ -361,6 +362,18 @@ class ThemeComposer
     //     return $menu->items;
     // }
 =======
+=======
+    public function getArticleModel(string $slug): Article|null
+    {
+        return Article::where('slug', $slug)->first();
+    }
+
+    public function getCategoryModel(string $slug): Category|null
+    {
+        return Category::where('slug', $slug)->first();
+    }
+
+>>>>>>> 0bd21d9 (Theme Composer getCategoryModel/getArticleModel)
     /**
      * Undocumented function.
      *
