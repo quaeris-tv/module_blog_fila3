366c47d30dfbb0a6b2b7b9f0a1945dc6a6334659
rasyidly <rasyid@sekeco.id>
2024-04-17 22:10:43 +0700

    adding responsive masonry

--- Resources/views/components/blocks/article_list/play_money_markets/list_of_markets.blade.php
+++ Resources/views/components/blocks/article_list/play_money_markets/list_of_markets.blade.php
@@ -1,6 +1,6 @@
 <div class="py-4">
     <div>
-        <div class="grid grid-cols-2 gap-5">
+        <div class="grid gap-5 md:grid-cols-2">
             @include('blog::components.blocks.article_list.play_money_markets.list_of_markets.article', ['articles' => $articles])
         </div>
     </div>
