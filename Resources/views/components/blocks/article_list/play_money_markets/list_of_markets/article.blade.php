9a4d1b503684f9c7fdc76e30c018cc772a4e6b57
rasyidly <rasyid@sekeco.id>
2024-04-17 22:03:44 +0700

    add masonry

--- Resources/views/components/blocks/article_list/play_money_markets/list_of_markets/article.blade.php
+++ Resources/views/components/blocks/article_list/play_money_markets/list_of_markets/article.blade.php
@@ -1,48 +1,52 @@
 {{-- {{ dddx($articles) }} --}}
-@foreach($articles as $article)
-    <article class="bg-white pt-6 lg:pl-6 pb-[18px] lg:pr-[18px] rounded-lg flex flex-col gap-6 border rounded">
-        <div class="pl-6 lg:pl-0">
-            
-            {{-- <template x-if="Boolean(market.event_start_date)"> --}}
-                @if($article->time_left_for_humans != null)
-                    <div class="flex items-center gap-2 text-sm font-medium text-blue-1">
-                        <svg width="8px" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="4" cy="4" r="4" fill="currentColor"></circle>
-                        </svg>
-                        {{-- In 16 hours and 05 minutes --}}
-                        {{ $article->time_left_for_humans }}
-                    </div>
-                @endif
-            {{-- </template> --}}
-            {{-- <a :href="'#'+market.slug" x-text="market.title" --}}
-            <a href="
-                {{-- {{ $article->url('show') }} --}}
-                {{ url('articles/'.$article->slug) }}
-                "
-                class="mt-1 sm:max-w-[310px] text-xl font-semibold text-neutral-4 block">
-                {{ $article->title }}
-            </a>
-            <!-- categories -->
-            @include('blog::components.blocks.article_list.play_money_markets.list_of_markets.categories')
-        </div>
+@foreach(collect($articles)->split(2) as $_articles)
+    <div class="grid gap-5">
+    @foreach($_articles as $article)
+        <article class="bg-white pt-6 lg:pl-6 pb-[18px] lg:pr-[18px] rounded-lg flex flex-col gap-6 border rounded">
+            <div class="pl-6 lg:pl-0">
+                
+                {{-- <template x-if="Boolean(market.event_start_date)"> --}}
+                    @if($article->time_left_for_humans != null)
+                        <div class="flex items-center gap-2 text-sm font-medium text-blue-1">
+                            <svg width="8px" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="4" cy="4" r="4" fill="currentColor"></circle>
+                            </svg>
+                            {{-- In 16 hours and 05 minutes --}}
+                            {{ $article->time_left_for_humans }}
+                        </div>
+                    @endif
+                {{-- </template> --}}
+                {{-- <a :href="'#'+market.slug" x-text="market.title" --}}
+                <a href="
+                    {{-- {{ $article->url('show') }} --}}
+                    {{ url('articles/'.$article->slug) }}
+                    "
+                    class="mt-1 sm:max-w-[310px] text-xl font-semibold text-neutral-4 block">
+                    {{ $article->title }}
+                </a>
+                <!-- categories -->
+                @include('blog::components.blocks.article_list.play_money_markets.list_of_markets.categories')
+            </div>
 
 
-        <!-- outcomes -->
-        {{-- questa blade è uguale a Blog\Resources\views\livewire\article\ratings-with-image\rating_with_image.blade.php --}}
-        {{-- @include('blog::components.blocks.article_list.play_money_markets.list_of_markets.article.outcomes', ['datas' => $article->ratings]) --}}
-        <livewire:article.ratings-with-image type="index" :ratings="$article->ratings" :wire:key="$article->uuid" :article_uuid="$article->uuid"/>
+            <!-- outcomes -->
+            {{-- questa blade è uguale a Blog\Resources\views\livewire\article\ratings-with-image\rating_with_image.blade.php --}}
+            {{-- @include('blog::components.blocks.article_list.play_money_markets.list_of_markets.article.outcomes', ['datas' => $article->ratings]) --}}
+            <livewire:article.ratings-with-image type="index" :ratings="$article->ratings" :wire:key="$article->uuid" :article_uuid="$article->uuid"/>
 
-        @include('blog::components.blocks.article_list.play_money_markets.list_of_markets.article.footer_info')
+            @include('blog::components.blocks.article_list.play_money_markets.list_of_markets.article.footer_info')
 
-        {{-- @include('blog::components.blocks.article_list.play_money_markets.list_of_markets.article.ratings') --}}
-        @if(Auth::check())
-            @php
-                $art = $article->toArray();
-            @endphp
-            <livewire:article.ratings-done :article_uuid="$article->uuid" :article_data="$art" wire:key="$article->uuid"/>
-        @endif
+            {{-- @include('blog::components.blocks.article_list.play_money_markets.list_of_markets.article.ratings') --}}
+            @if(Auth::check())
+                @php
+                    $art = $article->toArray();
+                @endphp
+                <livewire:article.ratings-done :article_uuid="$article->uuid" :article_data="$art" wire:key="$article->uuid"/>
+            @endif
 
-    </article>
+        </article>
+    @endforeach
+    </div>
 @endforeach
 
 
