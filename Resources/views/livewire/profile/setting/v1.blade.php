b7ccc3187b8ef69a556d29ff6b01bae31911f0bc
Malebestia <vair81@gmail.com>
2024-04-18 12:56:56 +0200

    fix urls

--- Resources/views/livewire/profile/setting/v1.blade.php
+++ Resources/views/livewire/profile/setting/v1.blade.php
@@ -11,13 +11,13 @@ if(!in_array($currentTab, ['profile', 'settings'])){
 <div class="p-6 space-y-8 bg-white lg:space-y-12 lg:p-10">
 	<ul class="flex items-center">
 		<li>
-			<a href="/it/pages/setting" class="flex items-center px-3 py-2 transition-colors space-x-1 border-b-2 {{ $currentTab == 'profile' ? 'border-blue-500' : 'border-gray-200'}}">
+			<a href="pages/setting" class="flex items-center px-3 py-2 transition-colors space-x-1 border-b-2 {{ $currentTab == 'profile' ? 'border-blue-500' : 'border-gray-200'}}">
 				<x-heroicon-o-user class="size-5"/>
 				<span>Profile</span>
 			</a>
 		</li>
 		<li>
-			<a href="/it/pages/setting?tab=settings" class="flex items-center px-3 py-2 transition-colors space-x-1 border-b-2 {{ $currentTab == 'settings' ? 'border-blue-500' : 'border-gray-200'}}">
+			<a href="pages/setting?tab=settings" class="flex items-center px-3 py-2 transition-colors space-x-1 border-b-2 {{ $currentTab == 'settings' ? 'border-blue-500' : 'border-gray-200'}}">
 				<x-heroicon-o-cog-6-tooth class="size-5"/>
 				<span>Setting</span>
 			</a>
