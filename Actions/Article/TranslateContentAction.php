bb3162b5a40e9c5e727294ebd66172e0c90adca1
Malebestia <vair81@gmail.com>
2024-04-21 18:03:41 +0200

    wip

--- Actions/Article/TranslateContentAction.php
+++ Actions/Article/TranslateContentAction.php
@@ -8,19 +8,47 @@ use Modules\Xot\Actions\GetModelByModelTypeAction;
 
 class TranslateContentAction
 {
-    public function execute(string $model_class, string $article_id, array $locales): void
+    public function execute(string $model_class, string $article_id, array $locales, array $data): void
     {
         $model = app(GetModelByModelTypeAction::class)->execute($model_class, $article_id);
 
-        $model_content = $model->toArray()['content_blocks'];
+        if($data['content_blocks']){
+            $model_content = $model->toArray()['content_blocks'];
 
-        // per ora do per scontato che la traduzione italiana esista
-        foreach ($locales as $locale) {
-            if (! isset($model_content[$locale])) {
-                $model_content[$locale] = $model_content['it'];
+            // per ora do per scontato che la traduzione italiana esista
+            foreach ($locales as $locale) {
+                if (! isset($model_content[$locale])) {
+                    $model_content[$locale] = $model_content['it'];
+                }
             }
+            $model->content_blocks = $model_content;
         }
-        $model->content_blocks = $model_content;
+
+        if($data['sidebar_blocks']){
+            $model_content = $model->toArray()['sidebar_blocks'];
+
+            // per ora do per scontato che la traduzione italiana esista
+            foreach ($locales as $locale) {
+                if (! isset($model_content[$locale])) {
+                    $model_content[$locale] = $model_content['it'];
+                }
+            }
+            $model->sidebar_blocks = $model_content;
+        }
+
+        if($data['footer_blocks']){
+            $model_content = $model->toArray()['footer_blocks'];
+
+            // per ora do per scontato che la traduzione italiana esista
+            foreach ($locales as $locale) {
+                if (! isset($model_content[$locale])) {
+                    $model_content[$locale] = $model_content['it'];
+                }
+            }
+            $model->footer_blocks = $model_content;
+        }
+
+
         $model->update();
     }
 }
