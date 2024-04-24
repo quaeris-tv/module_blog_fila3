ec8ccf9fd2ea7242b6d1d1d88a79e3f263e3b257
Malebestia <vair81@gmail.com>
2024-04-24 13:06:04 +0200

    start/wip componente livewire Credits

---
+++ Http/Livewire/Headernav/Credits.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Blog\Http\Livewire\Headernav;
+
+use Livewire\Component;
+use Modules\Blog\Models\Profile;
+use Modules\Xot\Actions\GetViewAction;
+use Illuminate\Contracts\Support\Renderable;
+
+class Credits extends Component
+{
+    public string $tpl = 'v1';
+    public Profile $profile;
+
+    public function mount(Profile $profile): void
+    {
+        $this->profile = $profile;
+    }
+
+    public function render(): Renderable
+    {
+        /**
+         * @phpstan-var view-string
+         */
+        $view = app(GetViewAction::class)->execute($this->tpl);
+
+        $credits = number_format($this->profile->credits, 0);
+
+        $view_params = [
+            'credits' => $credits
+        ];
+
+        return view($view, $view_params);
+    }
+}
