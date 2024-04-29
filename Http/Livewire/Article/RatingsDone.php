8f546e3117dc96e5deb9c3f57722aa7d1ad49b26
Malebestia <vair81@gmail.com>
2024-04-29 12:36:46 +0200

    .

--- Http/Livewire/Article/RatingsDone.php
+++ Http/Livewire/Article/RatingsDone.php
@@ -7,6 +7,7 @@ namespace Modules\Blog\Http\Livewire\Article;
 use Livewire\Component;
 use Livewire\Attributes\On;
 use Webmozart\Assert\Assert;
+use Modules\Blog\Models\Order;
 use Modules\Blog\Models\Article;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
@@ -55,7 +56,8 @@ class RatingsDone extends Component
         // dddx($this->user_ratings);
     }
 
-    public function getUserRatings(): array
+    // utilizzando il RatingMorph
+    public function getUserRatings_OLD(): array
     {
         $result = [];
 
@@ -84,6 +86,47 @@ class RatingsDone extends Component
                     'ratingId' => $rating['rating_id'],
                     'title' => 'not defined',
                     'credit' => $rating['value'],
+                    'image' => '#',
+                    'predict_victory' => 0,
+                ])->toArray();
+            }
+        }
+        $key_values = array_column($result, 'credit');
+        array_multisort($key_values, SORT_DESC, $result);
+
+        return $result;
+    }
+
+    // utilizzando Order
+    public function getUserRatings(): array
+    {
+        $result = [];
+
+        $user_ratings = Order::where('created_by', $this->user['id'])
+                ->where('model_id', $this->article_data['id'])
+                ->get()->toArray();
+
+        Assert::isArray($this->article_data['ratings']);
+        $ratings_options = collect($this->article_data['ratings']);
+
+        $percs = $this->getPercs();
+
+        foreach ($user_ratings as $key => $rating) {
+            Assert::isArray($rating);
+            $tmp = $ratings_options->where('id', $rating['rating_id'])->first();
+            if (null != $tmp) {
+                $result[] = RatingInfoData::from([
+                    'ratingId' => $rating['rating_id'],
+                    'title' => $tmp['title'],
+                    'credit' => $rating['credits'],
+                    'image' => $tmp['image'],
+                    'predict_victory' => $rating['credits'] * $percs[$rating['rating_id']],
+                ])->toArray();
+            } else {
+                $result[] = RatingInfoData::from([
+                    'ratingId' => $rating['rating_id'],
+                    'title' => 'not defined',
+                    'credit' => $rating['credits'],
                     'image' => '#',
                     'predict_victory' => 0,
                 ])->toArray();
