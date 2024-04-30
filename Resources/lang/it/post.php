10a9b76ffe66cdc626d5dc6826b9054ac6de864a
marco76tv <marco.sottana@gmail.com>
2024-04-30 20:48:57 +0200

    up

--- Resources/lang/it/post.php
+++ Resources/lang/it/post.php
@@ -10,27 +10,27 @@ return [
             'name' => 'Content',
         ],
     ],
-    // 'fields' => [
-    //     'name' => 'Nome',
-    //     'guard_name' => 'Guard',
-    //     'permissions' => 'Permessi',
-    //     'roles' => 'Ruoli',
-    //     'updated_at' => 'Aggiornato il',
-    //     'first_name' => 'Nome',
-    //     'last_name' => 'Cognome',
-    // ],
-    // 'actions' => [
-    //     'import' => [
-    //         'fields' => [
-    //             'import_file' => 'Seleziona un file XLS o CSV da caricare',
-    //         ],
-    //     ],
-    //     'export' => [
-    //         'filename_prefix' => 'Aree al',
-    //         'columns' => [
-    //             'name' => 'Nome area',
-    //             'parent_name' => 'Nome area livello superiore',
-    //         ],
-    //     ],
-    // ],
+     'fields' => [
+         'name' => 'Nome',
+         'guard_name' => 'Guard',
+         'permissions' => 'Permessi',
+         'roles' => 'Ruoli',
+         'updated_at' => 'Aggiornato il',
+         'first_name' => 'Nome',
+         'last_name' => 'Cognome',
+     ],
+     'actions' => [
+         'import' => [
+             'fields' => [
+                 'import_file' => 'Seleziona un file XLS o CSV da caricare',
+             ],
+         ],
+         'export' => [
+             'filename_prefix' => 'Aree al',
+             'columns' => [
+                 'name' => 'Nome area',
+                 'parent_name' => 'Nome area livello superiore',
+             ],
+         ],
+     ],
 ];
