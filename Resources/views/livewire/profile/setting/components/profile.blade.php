42e7ec956053b4d4c72c44d758838e5ed4688a3f
marco76tv <marco.sottana@gmail.com>
2024-04-30 22:42:23 +0200

    .

--- Resources/views/livewire/profile/setting/components/profile.blade.php
+++ Resources/views/livewire/profile/setting/components/profile.blade.php
@@ -17,14 +17,6 @@
                 <x-filament::loading-indicator class="w-5 h-5" wire:loading wire:target="editEmail"/>
                 <span>Email</span>
             </button>
-           <button wire:click="editPassword" class="flex items-center px-3 py-2 space-x-2 text-sm font-semibold transition rounded-lg text-nowrap ring-1 ring-gray-200 bg-gray-50 ring-inset hover:bg-gray-100 hover:no-underline">
-                <x-heroicon-o-pencil class="size-4"/>
-                <x-filament::loading-indicator class="w-5 h-5" wire:loading wire:target="editPassword"/>
-                <span>Password</span>
-            <button wire:click="editPassword" class="flex items-center px-3 py-2 space-x-2 text-sm font-semibold transition rounded-lg text-nowrap ring-1 ring-gray-200 bg-gray-50 ring-inset hover:bg-gray-100 hover:no-underline">
-                <x-heroicon-o-pencil class="size-4"/>
-                <x-filament::loading-indicator class="w-5 h-5" wire:loading wire:target="editPassword"/>
-                <span>Change Password</span>
             <button wire:click="editPassword" class="flex items-center px-3 py-2 space-x-2 text-sm font-semibold transition rounded-lg text-nowrap ring-1 ring-gray-200 bg-gray-50 ring-inset hover:bg-gray-100 hover:no-underline">
                 <x-heroicon-o-pencil class="size-4"/>
                 <x-filament::loading-indicator class="w-5 h-5" wire:loading wire:target="editPassword"/>
