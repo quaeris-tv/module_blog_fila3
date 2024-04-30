aa0bed5efdf9352c55e88cac147341de519f273c
marco76tv <marco.sottana@gmail.com>
2024-04-30 12:36:44 +0200

    up

--- Filament/Resources/ProfileResource.php
+++ Filament/Resources/ProfileResource.php
@@ -20,97 +20,13 @@ use Modules\Blog\Filament\Resources\ProfileResource\Pages;
 use Modules\Blog\Models\Profile;
 use Modules\User\Models\User;
 use Modules\Xot\Filament\Resources\XotBaseResource;
+use Modules\User\Filament\Resources\BaseProfileResource;
 
-class ProfileResource extends XotBaseResource
+class ProfileResource extends BaseProfileResource
 {
     use Translatable;
 
     protected static ?string $model = Profile::class;
-
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                // Forms\Components\TextInput::make('user_id'),
-                Forms\Components\TextInput::make('email'),
-                Forms\Components\TextInput::make('first_name'),
-                Forms\Components\TextInput::make('last_name'),
-                SpatieMediaLibraryFileUpload::make('photo_profile')
-                    // ->image()
-                    // ->maxSize(5000)
-                    // ->multiple()
-                    // ->enableReordering()
-                    ->openable()
-                    ->downloadable()
-                    ->columnSpanFull()
-                    // ->collection('avatars')
-                    // ->conversion('thumbnail')
-                    ->disk('uploads')
-                    ->directory('photos')
-                    ->collection('photo_profile'),
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                TextColumn::make('user.email'),
-                TextColumn::make('first_name'),
-                TextColumn::make('last_name'),
-                TextColumn::make('email'),
-                TextColumn::make('credits'),
-                SpatieMediaLibraryImageColumn::make('photo_profile')
-                    ->collection('photo_profile'),
-            ])
-            ->filters([
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
-                /* ---deve essere uno scherzone
-                Tables\Actions\Action::make('getCredits')
-                    // ->icon('heroicon-o-trash')
-                    ->requiresConfirmation()
-                    ->action(function (Profile $record, array $data) {
-                        dddx('wip');
-                        $added_credits_data = AddedCreditsData::from([
-                            'adminId' => Auth::id(),
-                            'profileId' => $record->user->id,
-                            'credit' => $data['credits'],
-                        ]);
-                        ProfileAggregate::retrieve($record->user->id)
-                            ->creditAdded($added_credits_data);
-                    })
-                    ->form([
-                        Forms\Components\TextInput::make('credits')
-                            ->numeric()
-                            ->label('Quanti crediti vuoi assegnare?'),
-                    ]),
-                */
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\BulkAction::make('refresh-profiles')
-                        ->requiresConfirmation()
-                        ->action(function (Collection $records) {
-                            $users = User::all();
-
-                            foreach ($users as $user) {
-                                Profile::firstOrCreate(
-                                    ['user_id' => $user->id, 'email' => $user->email],
-                                    ['credits' => 1000]
-                                );
-                            }
-                        }),
-                ]),
-            ])
-            ->emptyStateActions([
-            ]);
-    }
 
     public static function getRelations(): array
     {
