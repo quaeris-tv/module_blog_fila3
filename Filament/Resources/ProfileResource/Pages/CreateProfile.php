896bb252a0b6639219588ac44a78be2a31176cfd
marco76tv <marco.sottana@gmail.com>
2024-04-30 15:38:18 +0200

    up

--- Filament/Resources/ProfileResource/Pages/CreateProfile.php
+++ Filament/Resources/ProfileResource/Pages/CreateProfile.php
@@ -6,8 +6,9 @@ namespace Modules\Blog\Filament\Resources\ProfileResource\Pages;
 
 use Filament\Resources\Pages\CreateRecord;
 use Modules\Blog\Filament\Resources\ProfileResource;
+use Modules\User\Filament\Resources\BaseProfileResource\Pages\CreateProfile as UserCreateProfile;
 
-class CreateProfile extends CreateRecord
+class CreateProfile extends UserCreateProfile
 {
     protected static string $resource = ProfileResource::class;
 }
--- Filament/Resources/ProfileResource/Pages/EditProfile.php
+++ Filament/Resources/ProfileResource/Pages/EditProfile.php
@@ -7,8 +7,9 @@ namespace Modules\Blog\Filament\Resources\ProfileResource\Pages;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Modules\Blog\Filament\Resources\ProfileResource;
+use Modules\User\Filament\Resources\BaseProfileResource\Pages\EditProfile as UserEditProfile;
 
-class EditProfile extends EditRecord
+class EditProfile extends UserEditProfile
 {
     protected static string $resource = ProfileResource::class;
 
