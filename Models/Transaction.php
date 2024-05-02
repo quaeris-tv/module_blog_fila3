56d64f25ebe2cdb466c2a1e3da1b70db0925bd03
Malebestia <vair81@gmail.com>
2024-05-02 19:18:04 +0200

    add transactions table

---
+++ Models/Transaction.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Blog\Models;
+
+class Transaction extends BaseModel
+{
+    /** @var string */
+    protected $connection = 'blog';
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = ['date', 'model_type', 'model_id', 'user_id', 'credits', 'note'];
+    
+}
