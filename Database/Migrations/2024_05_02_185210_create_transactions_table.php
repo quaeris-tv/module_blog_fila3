b070c9d65d90863697920963406af9dba799358f
Malebestia <vair81@gmail.com>
2024-05-02 19:18:04 +0200

    add transactions table

---
+++ Database/Migrations/2024_05_02_185210_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Modules\User\Models\User;
+use Illuminate\Database\Schema\Blueprint;
+use Modules\Xot\Database\Migrations\XotBaseMigration;
+
+class CreateTransactionsTable extends XotBaseMigration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $this->tableCreate(
+            static function (Blueprint $table): void {
+                $table->id();
+                $table->nullableMorphs('model');
+                $table->integer('credits')->nullable();
+                $table->foreignIdFor(User::class, 'user_id')->nullable();
+                $table->text('note')->nullable();
+                $table->datetimetz('date')->nullable();
+
+            });
+
+
+                    // -- UPDATE --
+        $this->tableUpdate(
+            function (Blueprint $table): void {
+                // if (! $this->hasColumn('rating_id')) {
+                //     $table->integer('rating_id')->after('article_id');
+                // }
+
+                $this->updateTimestamps(table: $table, hasSoftDeletes: true);
+            }
+        );
+    }
+};
