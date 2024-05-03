ce2e68e3d156df4a4c2802adbf8348643692ef04
marco76tv <marco.sottana@gmail.com>
2024-05-03 15:54:38 +0200

    up

--- Filament/Fields/PageContent.php
+++ Filament/Fields/PageContent.php
@@ -19,6 +19,7 @@ use Modules\UI\Filament\Blocks\Hero;
 use Modules\UI\Filament\Blocks\Paragraph;
 use Modules\UI\Filament\Blocks\Slider;
 use Modules\UI\Filament\Blocks\Title;
+use Modules\UI\Filament\Blocks\Hero;
 
 class PageContent
 {
@@ -28,6 +29,7 @@ class PageContent
     ): Builder {
         return Builder::make($name)
             ->blocks([
+                
                 Title::make(context: $context),
                 Paragraph::make(context: $context),
                 Image::make(context: $context),
