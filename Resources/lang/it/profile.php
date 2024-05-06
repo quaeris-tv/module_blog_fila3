4da552e6c6d9cc8bd5b22dd79b77610f100dde55
marco76tv <marco76tv@users.noreply.github.com>
2024-05-06 13:30:22 +0000

    Lint

--- Resources/lang/it/profile.php
+++ Resources/lang/it/profile.php
@@ -32,6 +32,6 @@ return [
         'option' => 'Opzione',
         'welcome' => 'Regalo di benvenuto',
         'rating_article' => 'Scommessa effettuata',
-        'rating_article_winner' => 'Vincente'
+        'rating_article_winner' => 'Vincente',
     ],
 ];
