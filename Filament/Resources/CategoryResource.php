4acee8aba1613c8245b3119a83a6e59661043990
Malebestia <vair81@gmail.com>
2024-07-08 17:37:27 +0200

    .

--- Filament/Resources/CategoryResource.php
+++ Filament/Resources/CategoryResource.php
@@ -67,10 +67,10 @@ class CategoryResource extends XotBaseResource
                 // ->conversion('thumbnail')
                 ->disk('uploads')
                 ->directory('photos'),
-            \Guava\FilamentIconPicker\Forms\IconPicker::make('icon')
-                ->helperText('Visualizza le icone disponibili di https://heroicons.com/')
-                ->columnSpanFull()
-                ->layout(\Guava\FilamentIconPicker\Layout::ON_TOP),
+            // \Guava\FilamentIconPicker\Forms\IconPicker::make('icon')
+            //     ->helperText('Visualizza le icone disponibili di https://heroicons.com/')
+            //     ->columnSpanFull()
+            //     ->layout(\Guava\FilamentIconPicker\Layout::ON_TOP),
         ];
     }
 
