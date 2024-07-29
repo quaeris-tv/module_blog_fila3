be24758c54f43bcad8c1ab41648885f468a1df06
marco76tv <marco76tv@users.noreply.github.com>
2024-07-29 09:13:23 +0000

    Lint

--- Tests/Unit/SumTest.php
+++ Tests/Unit/SumTest.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
-function sum($a, $b) {
-   return $a + $b;
+function sum($a, $b)
+{
+    return $a + $b;
 }
 
 it('sum', function () {
-   $result = sum(1, 2);
- 
-   expect($result)->toBe(3);
+    $result = sum(1, 2);
+
+    expect($result)->toBe(3);
 });
