6ae547d1cf2faa5d64d19793ee564debf51f304c
ciosper <gianmarco.minto@gmail.com>
2024-07-30 17:15:28 +0200

    conflict

--- Tests/Unit/SumTest.php
+++ Tests/Unit/SumTest.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-<<<<<<< HEAD
 
 function sum($a, $b)
 {
@@ -9,9 +8,6 @@ function sum($a, $b)
 }
 
 it('sum', function () {
-=======
-test('sum', function () {
->>>>>>> 48f3485a273067c3f93c4ca332fb4e4ec5d5a162
     $result = sum(1, 2);
 
     expect($result)->toBe(3);
