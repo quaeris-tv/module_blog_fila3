cb6e5c9d1b546fea73c716899812946b7833bd55
Malebestia <vair81@gmail.com>
2024-12-04 17:29:55 +0100

    fix(Resources/lang/it/article.php): update translations for article navigation and fields

--- Filament/Blocks/ArticleCard.php
+++ Filament/Blocks/ArticleCard.php
@@ -4,13 +4,27 @@ declare(strict_types=1);
 
 namespace Modules\Blog\Filament\Blocks;
 
-use Filament\Forms\Components\Builder\Block;
+use Modules\Blog\Models\Article;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
-use Modules\Blog\Models\Article;
+use Filament\Forms\Components\Builder\Block;
+use Modules\Xot\Filament\Blocks\XotBaseBlock;
 
-class ArticleCard
+class ArticleCard // extends XotBaseBlock
 {
+
+    
+    // public static function getBlockSchema(): array
+    // {
+    //     return [
+    //         Select::make('article_id')
+    //             ->options(Article::published()->orderBy('title')->pluck('title', 'id'))
+    //             ->required(),
+
+    //         TextInput::make('text')
+    //     ];
+    // }
+
     public static function make(
         string $name = 'article_card',
         string $context = 'form',
--- Resources/lang/it/article.php
+++ Resources/lang/it/article.php
@@ -1,85 +1,62 @@
-<?php
-
-declare(strict_types=1);
-
-/*
-use Illuminate\Support\Facades\Auth;
-use Modules\Blog\Models\Profile;
-use Webmozart\Assert\Assert;
-*/
-// NNOO PERCHE SE UNO E' SLOGGATO DA ERRORE !!
-/*
-Assert::notNull($user = Auth::user(),'['.__LINE__.']['.__FILE__.']');
-Assert::notNull($profile = $user->profile,'['.__LINE__.']['.__FILE__.']');
-Assert::isInstanceOf($profile, Profile::class);
-*/
-return [
-    'navigation' => [
-        'name' => 'Articolo',
-        'plural' => 'Articoli',
-        'group' => [
-            'name' => 'Content',
-        ],
-    ],
-
-    'rating' => [
-        'no_import' => 'Nessuna cifra inserita',
-        'import_zero' => 'Nessuna cifra inserita',
-        'import_min' => 'Hai superato la cifra di :credits: crediti',
-        'no_choice' => 'Nessuna opzione scelta',
-    ],
-    'single_expired' => 'Scaduto',
-    'expired' => 'Articolo scaduto, non si possono fare più scommesse',
-    'no_vote' => 'Siamo spiacenti, ma questa votazione è chiusa da :TIME, per favore prova a fare un altra previsione',
-    'your_bet' => 'La tua previsione',
-    'your_amount' => 'Previsione',
-    'if_win' => 'Se vinci',
-
-    'fields' => [
-        'id' => [
-            'label' => 'id',
-        ],
-        'title' => [
-            'label' => 'Titolo',
-        ],
-        'category' => [
-            'title' => ['label' => 'Categoria'],
-        ],
-        'published_at' => [
-            'label' => 'Publicato il',
-        ],
-        'closed_at' => [
-            'label' => 'Chiuso il',
-        ],
-        'rewarded_at' => [
-            'label' => 'Premiato il',
-        ],
-        'is_featured' => [
-            'label' => 'In primo piano',
-        ],
-    ],
-
-    // 'fields' => [
-    //     'name' => 'Nome',
-    //     'guard_name' => 'Guard',
-    //     'permissions' => 'Permessi',
-    //     'roles' => 'Ruoli',
-    //     'updated_at' => 'Aggiornato il',
-    //     'first_name' => 'Nome',
-    //     'last_name' => 'Cognome',
-    // ],
-    // 'actions' => [
-    //     'import' => [
-    //         'fields' => [
-    //             'import_file' => 'Seleziona un file XLS o CSV da caricare',
-    //         ],
-    //     ],
-    //     'export' => [
-    //         'filename_prefix' => 'Aree al',
-    //         'columns' => [
-    //             'name' => 'Nome area',
-    //             'parent_name' => 'Nome area livello superiore',
-    //         ],
-    //     ],
-    // ],
-];
+<?php return array (
+  'navigation' => 
+  array (
+    'name' => 'Articolo',
+    'plural' => 'Articoli',
+    'group' => 
+    array (
+      'name' => 'Content',
+    ),
+  ),
+  'rating' => 
+  array (
+    'no_import' => 'Nessuna cifra inserita',
+    'import_zero' => 'Nessuna cifra inserita',
+    'import_min' => 'Hai superato la cifra di :credits: crediti',
+    'no_choice' => 'Nessuna opzione scelta',
+  ),
+  'single_expired' => 'Scaduto',
+  'expired' => 'Articolo scaduto, non si possono fare più scommesse',
+  'no_vote' => 'Siamo spiacenti, ma questa votazione è chiusa da :TIME, per favore prova a fare un altra previsione',
+  'your_bet' => 'La tua previsione',
+  'your_amount' => 'Previsione',
+  'if_win' => 'Se vinci',
+  'fields' => 
+  array (
+    'id' => 
+    array (
+      'label' => 'id',
+    ),
+    'title' => 
+    array (
+      'label' => 'Titolo',
+    ),
+    'category' => 
+    array (
+      'title' => 
+      array (
+        'label' => 'Categoria',
+      ),
+    ),
+    'published_at' => 
+    array (
+      'label' => 'Publicato il',
+    ),
+    'closed_at' => 
+    array (
+      'label' => 'Chiuso il',
+    ),
+    'rewarded_at' => 
+    array (
+      'label' => 'Premiato il',
+    ),
+    'is_featured' => 
+    array (
+      'label' => 'In primo piano',
+    ),
+    'Categoria' => 
+    array (
+      'label' => 'Categoria',
+    ),
+  ),
+);
