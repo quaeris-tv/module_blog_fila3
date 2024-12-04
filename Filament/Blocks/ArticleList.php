fbbd5e206ab6d6c709faa85fdbb11b3bbfbadc61
marco76tv <marco.sottana@gmail.com>
2024-12-04 12:24:59 +0100

    .

--- Filament/Blocks/ArticleList.php
+++ Filament/Blocks/ArticleList.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Blog\Filament\Blocks;
 
-use Modules\Blog\Models\Article;
-use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Builder\Block;
 use Modules\Xot\Filament\Blocks\XotBaseBlock;
-use Modules\Xot\Actions\View\GetViewsSiblingsAndSelfAction;
-use Modules\Xot\Actions\Filament\Block\GetViewBlocksOptionsByTypeAction;
 
 class ArticleList extends XotBaseBlock
 {
@@ -40,14 +36,7 @@ class ArticleList extends XotBaseBlock
                     ->required(),
                 */
                 TextInput::make('limit'),
-                /*
-                Select::make('_tpl')
-                    
-                    ->options($views)
-                    ->default('v1')
-                    ->required(),
-                */
-                    ];
+        ];
                
     }
 
